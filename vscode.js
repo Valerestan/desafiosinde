d5d89a6c8c71dfcfb9dec210e2ca18b5c9bdb98e
Vale Restan <valeresmune10@gmail.com>
2025-01-21 21:40:13 -0500

    avances

--- vscode.js
+++ vscode.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //alert("funciona");
 
 //16. Solicitar nombre y apellido al usuario y mostrarlos en una ventana emergente.
@@ -18,7 +20,44 @@
 Ej entrada: Pepita Pérez 16
 Ej salida: Pérez Pepita tiene 16 años.*/
 
-const datos = prompt("¿Cual es tu nombre?");
+/*const datos = prompt("¿Cual es tu nombre?");
 const datos2 = prompt("¿cual es tu apellido?");
 const datos3 = prompt("¿cual es tu edad?");
-document.write(datos + " " + datos2 + " tienes " + datos3 + " años.");
+document.write(datos + " " + datos2 + " tienes " + datos3 + " años."); */
+
+/*19. Solicitar un nombre al usuario y:
+- Mostrar en una ventana emergente el tipo de dato ingresado entre comillas sencillas
+- Mostrar en la interfaz de usuario el nombre ingresado entre comillas dobles
+Ej entrada: Pepita
+Es salida:
+- El tipo de dato ingresado es: 'string'
+- El valor ingresado es: "Pepita"*/
+
+/*const datos = prompt("¿Cual es tu nombre?");
+alert("El tipo de dato ingresado es: '" + typeof datos + "'");
+
+document.write('El valor ingresado es: "' + datos + '"'); */
+
+/*20. Solicitar al usuario un nombre, una edad y que responda si está activo o no
+- Mostrar en la consola el tipo de dato ingresado por cada entrada
+- Mostrar en la interfaz de usuario el nombre ingresado entre comillas sencillas
+- Mostrar en una ventana emergente si está autorizado o no entre comillas dobles
+Ej entrada: Pepita 20 si
+Ej salida:
+- El tipo de dato ingresado para nombre es: 'string' ...
+- El nombre ingresado es: 'Pepita'
+- "Si" está autorizado*/
+
+const datos = prompt("¿Cual es tu nombre?");
+const datos1 = prompt("¿cual es tu edad?");
+const datos2 = prompt("¿estas activo?");
+
+console.log("El tipo de dato ingresado para nombre es: '" + typeof datos + "'");
+console.log("El tipo de dato ingresado para edad es: '" + typeof datos1 + "'");
+console.log(
+  "El tipo de dato ingresado para activo es: '" + typeof datos2 + "'"
+);
+
+document.write("El nombre ingresado es: '" + datos + "'");
+
+alert('"' + datos2 + '"' + " " + "está autorizado");
